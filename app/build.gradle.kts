93ba00166452735914494d788df726ee0e4e8d97
Chamin Im <ckals413@naver.com>
2024-09-12 21:51:56 +0900

    FEAT: 데이터 바인딩 문제 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,15 +18,22 @@ android {
     }
 
 //이렇게 해서 오류가 계속 나던거였어 ; 계속 이렇게 했었는데, 버전이 다운그레이드 된건가,,,
-//    // AndroidStudio 4.0 ~
-//    buildFeatures{
+    // AndroidStudio 4.0 ~
+//    buildFeatures {
 //        viewBinding = true
 //        compose = true
 //        dataBinding = true
+//    }
 
-    // 이렇게 데이터 바인딩... 
-    dataBinding{
-        enable = true
+//    // 이건 오류는 안나는데, 데이터 바인딩이 안됨
+//    dataBinding{
+//        enable = true
+//    }
+
+    // 이렇게 해서 데이터 바인딩. . .
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
     }
 
     buildTypes {
@@ -57,4 +64,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // CameraX
+    val camerax_version = "1.4.0-beta02"
+    implementation ("androidx.camera:camera-core:${camerax_version}")
+    implementation ("androidx.camera:camera-camera2:${camerax_version}")
+    implementation ("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation ("androidx.camera:camera-video:${camerax_version}")
+    implementation ("androidx.camera:camera-view:${camerax_version}")
+    implementation ("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    implementation ("androidx.camera:camera-extensions:${camerax_version}")
+    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
+
 }
